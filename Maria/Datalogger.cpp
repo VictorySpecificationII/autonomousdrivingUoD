0f10562a98a122a3baba2dbb866bb276763df2fe
Antreas Christofi <victoryspecificationii@gmail.com>
2020-04-12 15:32:57 +0300

    refactored code

--- Maria/Datalogger.cpp
+++ Maria/Datalogger.cpp
@@ -20,17 +20,8 @@
 
 using namespace std;
 
-int Datalogger::NewLog()
-{
-  
-  // Create and open a text file
-  ofstream MyFile("Log.txt");
-
-   
-  // Write to the file
-  MyFile << "Beginning of log...\n";
-
-   // current date/time based on current system
+char* TimeStamp(){
+	// current date/time based on current system
    time_t now = time(0);
    
    // convert now to string form
@@ -41,17 +32,33 @@ int Datalogger::NewLog()
    // convert now to tm struct for UTC
    tm *gmtm = gmtime(&now);
    dt = asctime(gmtm);
-   MyFile << "The UTC date and time is: "<< dt << endl;
+   return dt;
+}
+
+int Datalogger::NewLog()
+{
+  
+  // Create and open a text file
+  ofstream MyFile("Log.txt");
+
+   
+  // Write to the file
+  MyFile << "Beginning of log...\n";
+
+  char* dt = TimeStamp();
+   
+  MyFile << "The UTC date and time is: "<< dt << endl;
 
   // Close the file
-  //MyFile.close();
+  MyFile.close();
   return 0;
 }
 
 int Datalogger::AppendToLog(string tag, string data){
 
   ofstream MyFile("Log.txt", std::ios::app);
-  MyFile << tag << " - " << data << ", "; 
+  char* dt = TimeStamp();
+  MyFile << dt << tag << " - " << data << ", "; 
   return 0;
 
 }
@@ -64,12 +71,8 @@ int main(){
 	
 	Logger.NewLog();
 	//trying to turn it into a CSV, sort at the end
+	for(int i = 0; i < 10; i++){
 	Logger.AppendToLog("Tag", "Appended Data");
-	Logger.AppendToLog("Tag", "Appended Data");
-	Logger.AppendToLog("Tag", "Appended Data");
-	Logger.AppendToLog("Tag", "Appended Data");
-	Logger.AppendToLog("Tag", "Appended Data");
-	Logger.AppendToLog("Tag", "Appended Data");
-
+	}
 	return 0; 
 } 
